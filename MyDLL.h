880e5bf3b6b53093638e5cf66eee0a1140b1de2e
Vasco <tiagochanpestana@ua.pt>
2025-03-11 11:05:51 +0000

    MyDLL.h

--- MyDLL.h
+++ MyDLL.h
@@ -1,12 +1,31 @@
 #ifndef MYDLL_H
 #define MYDLL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdint.h>
 
+typedef struct Node {
+    uint16_t key;
+    unsigned char *number;
+    struct Node *prev;
+    struct Node *next;
+} Node;
 
-void MyDLLInit();
+typedef struct {
+    Node *head;
+    Node *tail;
+    size_t max_elements;
+    size_t element_size;
+    size_t count;
+} DLL;
+
+void MyDLLInit(DLL *list, size_t max_elements, size_t element_size);
 int MyDLLInsert();
 int MyDLLRemove();
 unsigned char *MyDLLFind();
 unsigned char *MyDLLFindNext();
 unsigned char *MyDLLFindPrevious();
+
+#endif
